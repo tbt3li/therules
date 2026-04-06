412287563d9d6c7924569f8bdaa7ffa57da8efea
tbt3li <crtaso92@gmail.com>
2026-04-06 09:46:34 +0500

    Update footer.js

--- footer.js
+++ footer.js
@@ -2,26 +2,35 @@ document.write(`
 </script>
     <footer id="footer" class="footer dark-background">
     <div class="container">
-	  <div style="margin: 0 0 30px 0;">
-		<div style="float: left;">
-			<img src="assets/img/wlogo.png" style="max-height: 150px;" alt="" class="img-fluid flex-shrink-0">
+	  <div class="container mb-4">
+		  <div class="row align-items-center">
+
+			<!-- Left 25% -->
+			<div class="col-md-3 text-md-start text-center mb-3 mb-md-0">
+			  <img src="assets/img/wlogo.png" class="img-fluid" style="max-height:150px;">
+			</div>
+
+			<!-- Center 50% -->
+			<div class="col-md-6 text-center">
+			  <h4 class="sitename">THE<strong style="color:#ef6603;"> Great Family Gang</strong>
+			  </h4>
+
+			  <p>We committed to leading with strategy, unity, respect and dedication to ensure our clan's success.</p>
+
+			  <div class="social-links d-flex justify-content-center">
+				<a href="https://discord.com/users/cut3mimi"><i class="bi bi-discord"></i></a>
+			  </div>
+			</div>
+
+			<!-- Right 25% -->
+			<div class="col-md-3 text-md-end text-center mt-3 mt-md-0">
+			  <a href="https://info.flagcounter.com/cWwa">
+				<img src="https://s01.flagcounter.com/count2/cWwa/bg_FFFFFF/txt_000000/border_CCCCCC/columns_3/maxflags_12/viewers_Visitors/labels_0/pageviews_1/flags_0/percent_0/" class="img-fluid">
+			  </a>
+			</div>
+
+		  </div>
 		</div>
-	  <div>
-      <h4 class="sitename"><strong style="color:#ef6603;">THE</strong> Great Family Gang</h4>
-      <p>We committed to leading with strategy, unity, respect and dedication to ensure our clan's success.</p>
-      <div class="social-links d-flex justify-content-center">
-        <a href="https://discord.com/users/cut3mimi"><i class="bi bi-discord"></i></a>
-      </div>
-	  </div>
-	  </div>
-      <div class="container">
-        <div class="copyright">
-          <span>Copyright 2026 All Rights Reserved</span>
-        </div>
-        <div class="credits">
-          Designed by <a href="\"><text style="font-size:16px; font-style:italic;">웃 T3li</text></a> Distributed By <a href="\">THE</a>
-        </div>
-      </div>
     </div>
   </footer>
 
